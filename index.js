c818ca535ff9af1fc9590f1608fb46d3acc6f322
Ertuğrul Evrensel <ertugrulevrensel@gmail.com>
2022-09-07 00:41:34 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import { products } from "./product.js";
+$(document).ready(function () {
+  var owl = $("#owl-demo");
+
+  owl.owlCarousel({
+    nav: true,
+    dots: false,
+    margin: 30,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 2.5,
+      },
+      600: {
+        items: 2.5,
+      },
+      1000: {
+        items: 2.5,
+      },
+    },
+    navText: [
+      "",
+      "<div class='d-flex'>  <div class='d-flex arrow-content'>    <img src='./assets/arrow.png' />  </div></div>",
+    ],
+  });
+});
+products.forEach((product) => {
+  $("#owl-demo").append(
+    `<div class='product-area d-flex flex-d-col'>    <img src='${product.image}' />    <p class='product-name'>${product.productName}</p>    <div class='product-description d-flex justify-s-between'>      <p><b>${product.price}</b></p>      <div class='d-flex color-choice-area'>        <div data-selected='selected' class='d-flex color-choice-div'>          <span            class='color-choice'            style='background-color: ${product.color}'          ></span>        </div>        <div class='d-flex color-choice-div'>          <span            class='color-choice'            style='background-color: #cc9999'          ></span>        </div>        <div class='d-flex color-choice-div'>          <span            class='color-choice'            style='background-color: #cb99cc'          ></span>        </div>        <div class='d-flex color-choice-div'>          <span            class='color-choice'            style='background-color: #a6cc99'          ></span>        </div>      </div>    </div>  </div>`
+  );
+});
---
+++ product.js
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 1,
+    productName: "365 Signature Hoodie",
+    image: "./assets/clothes1.png",
+    price: "€33,95",
+    color: "#99C3CC",
+  },
+  {
+    id: 2,
+    productName: "Organic Skinnt High Waist Jeans",
+    image: "./assets/clothes2.png",
+    price: "€33,95",
+    color: "#99C3CC",
+  },
+  {
+    id: 3,
+    productName: "Organic Skinny High Waist Jeans",
+    image: "./assets/clothes3.png",
+    price: "€33,95",
+    color: "#99C3CC",
+  },
+];
