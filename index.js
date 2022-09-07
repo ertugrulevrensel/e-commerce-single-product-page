6e17ea3a1f449edfa219157e93df86a704d6e13d
Ertuğrul Evrensel <ertugrulevrensel@gmail.com>
2022-09-07 10:59:17 +0300

    clear comment line

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { products } from "./product.js";
 $(document).ready(function () {
   var owl = $("#owl-demo");
 
+  // owlCarousel setting
+
   owl.owlCarousel({
     nav: true,
     dots: false,
@@ -18,6 +20,9 @@ $(document).ready(function () {
     ],
   });
 });
+
+// add each product to the page
+
 products.forEach((product) => {
   $("#owl-demo").append(
     `<div class='product-area d-flex flex-d-col'>    <img src='${product.image}' />    <p class='product-name'>${product.productName}</p>    <div class='product-description d-flex justify-s-between'>      <p><b>${product.price}</b></p>      <div class='d-flex color-choice-area'>        <div data-selected='selected' class='d-flex color-choice-div'>          <span            class='color-choice'            style='background-color: ${product.color}'          ></span>        </div>        <div class='d-flex color-choice-div'>          <span            class='color-choice'            style='background-color: #cc9999'          ></span>        </div>        <div class='d-flex color-choice-div'>          <span            class='color-choice'            style='background-color: #cb99cc'          ></span>        </div>        <div class='d-flex color-choice-div'>          <span            class='color-choice'            style='background-color: #a6cc99'          ></span>        </div>      </div>    </div>  </div>`
